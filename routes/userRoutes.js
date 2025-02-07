2867fcf9b5839860616be21019ff8bb9044a4f82
Jayasurya-S <jayasurya4060@gmail.com>
2025-02-07 14:21:56 +0530

    fix

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,7 @@ const { registerAuth, loginAuth, verifyToken } = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/register", registerAuth, userRegister);
-router.get("/login", loginAuth, userLogin);
+router.post("/login", loginAuth, userLogin);
 router.get("/user", verifyToken, getUserInfo);
 
 module.exports = router;
